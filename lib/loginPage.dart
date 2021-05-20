b01a76dd1765765546127315d4dac47f43d43985
vishaldhiman1717 <vishaldhiman7788@gmail.com>
2021-05-20 18:04:59 +0530

    first commit

---
+++ lib/loginPage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+      children: [
+        Image.asset("assets/images/demo.png"),
+        SizedBox(height: 20),
+        Text("LOGIN", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0 , horizontal: 32.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(hintText: "Enter Username",labelText: "Username")
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(hintText: "Enter Password",labelText: "Password")
+              ),
+              SizedBox(
+                height: 20.0
+              ),
+
+              ElevatedButton( 
+                child: Text("login"),
+                style: TextButton.styleFrom(),
+                onPressed: (){
+                      print("hello vishal");
+                },
+                )
+            ],
+          ),
+        )
+      ],
+      ),
+      );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/loginPage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
+      home: Material(child: LoginPage(),),
+    );
+  }
+}
